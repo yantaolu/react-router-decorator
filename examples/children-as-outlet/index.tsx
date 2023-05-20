95d1df9b8cf5c84aa87c0e402c860bae38a29bfa
Yantao Lu <luyantaoforever@163.com>
2023-05-20 22:44:40 +0800

    style: 💄 修改 childrenAsOutlet 示例

--- examples/children-as-outlet/index.tsx
+++ examples/children-as-outlet/index.tsx
@@ -4,4 +4,8 @@ import './pages/home';
 import './pages/info';
 import './pages/user';
 
-renderApp(document.getElementById('app') as HTMLElement, { type: 'history', withPageWrapper: false });
+renderApp(document.getElementById('app') as HTMLElement, {
+  type: 'history',
+  withPageWrapper: false,
+  childrenAsOutlet: true,
+});
